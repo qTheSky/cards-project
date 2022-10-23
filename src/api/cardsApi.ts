8e9b5a0b55c4eb942c87d1cba36ed8b88df00ae2
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-10-23 15:32:04 +0300

    implemented upload card answer image

--- src/api/cardsApi.ts
+++ src/api/cardsApi.ts
@@ -1,83 +1,84 @@
 import {instance} from 'api/instance';
 
 export const cardsAPI = {
-		getCards(params: CardsSearchParams) {
-				return instance.get<GetCardsResponse>('cards/card', {params})
-		},
-		createCard(params: CreateCardType) {
-				return instance.post('cards/card', {card: params})
-		},
-		deleteCard(cardId: string) {
-				return instance.delete(`cards/card/?id=${cardId}`)
-		},
-		editCard(params: EditCardType) {
-				return instance.put('cards/card', {card: params})
-		},
-		gradeCard(gradeParams: GradeCardParamsType) {
-				return instance.put<{ updatedGrade: UpdatedGradeType }>('cards/grade', gradeParams)
-		}
+    getCards(params: CardsSearchParams) {
+        return instance.get<GetCardsResponse>('cards/card', {params})
+    },
+    createCard(params: CreateCardType) {
+        return instance.post('cards/card', {card: params})
+    },
+    deleteCard(cardId: string) {
+        return instance.delete(`cards/card/?id=${cardId}`)
+    },
+    editCard(params: EditCardType) {
+        return instance.put('cards/card', {card: params})
+    },
+    gradeCard(gradeParams: GradeCardParamsType) {
+        return instance.put<{ updatedGrade: UpdatedGradeType }>('cards/grade', gradeParams)
+    }
 }
 export type GetCardsResponse = {
-		cards: CardType[]
-		cardsTotalCount: number
-		maxGrade: number
-		minGrade: number
-		packCreated: Date
-		packName: string
-		packPrivate: boolean
-		packUpdated: Date
-		packUserId: string
-		page: number
-		pageCount: number
-		packDeckCover:string
+    cards: CardType[]
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    packCreated: Date
+    packName: string
+    packPrivate: boolean
+    packUpdated: Date
+    packUserId: string
+    page: number
+    pageCount: number
+    packDeckCover: string
 }
 export type CardsSearchParams = {
-		cardAnswer: string
-		cardQuestion: string
-		cardsPack_id: string
-		min: number
-		max: number
-		sortCards: string
-		page: number
-		pageCount: number
+    cardAnswer: string
+    cardQuestion: string
+    cardsPack_id: string
+    min: number
+    max: number
+    sortCards: string
+    page: number
+    pageCount: number
 }
 export type CardType = {
-		answer: string
-		answerImg: string
-		answerVideo: string
-		cardsPack_id: string
-		comments: string
-		created: Date
-		grade: number
-		question: string
-		questionImg: string
-		questionVideo: string
-		rating: number
-		shots: number
-		type: string
-		updated: Date
-		user_id: string
-		_id: string
+    answer: string
+    answerImg: string
+    answerVideo: string
+    cardsPack_id: string
+    comments: string
+    created: Date
+    grade: number
+    question: string
+    questionImg: string
+    questionVideo: string
+    rating: number
+    shots: number
+    type: string
+    updated: Date
+    user_id: string
+    _id: string
 }
 export type CreateCardType = {
-		cardsPack_id: string
-		question: string
-		answer: string
+    cardsPack_id: string
+    question?: string
+    answer: string
+    questionImg?: string
 }
 export type EditCardType = {
-		_id: string
-		question: string
-		answer: string
+    _id: string
+    question: string
+    answer: string
 }
 export type GradeCardParamsType = {
-		grade: number
-		card_id: string
+    grade: number
+    card_id: string
 }
 export type UpdatedGradeType = {
-		_id: string
-		cardsPack_id: string
-		card_id: string
-		user_id: string
-		grade: number
-		shots: number
+    _id: string
+    cardsPack_id: string
+    card_id: string
+    user_id: string
+    grade: number
+    shots: number
 }
--- src/features/Cards/CardsModals/AddCardModal.tsx
+++ src/features/Cards/CardsModals/AddCardModal.tsx
@@ -1,57 +1,114 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {BasicModal} from 'features/Modals/BasicModal';
-import {Button, TextField} from '@mui/material';
+import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from '@mui/material';
 import {useAppDispatch} from 'app/store';
 import {createCard, fetchCards} from 'features/Cards/cards-reducer';
 
 
 interface IProps {
-		packId: string
+    packId: string
 }
 
 export const AddCardModal = ({packId}: IProps) => {
-		const dispatch = useAppDispatch()
-		const [open, setOpen] = useState(false)
-		const [question, setQuestion] = useState('')
-		const [answer, setAnswer] = useState('')
+    const dispatch = useAppDispatch()
+    const [open, setOpen] = useState(false)
+    const [question, setQuestion] = useState('')
+    const [answer, setAnswer] = useState('')
+    const [questionFormat, setQuestionFormat] = useState('Text')
+    const [questionImg, setQuestionImg] = useState('')
 
 
-		const handleSave = async () => {
-				await dispatch(createCard({cardsPack_id: packId, answer, question}))
-				await dispatch(fetchCards(packId))
-				setOpen(false)
-				setQuestion('')
-				setAnswer('')
-		}
+    const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length) {
+            const file = e.target.files[0]
 
-		return (
-				<div>
-						<Button variant="contained" onClick={() => setOpen(true)}>
-								Add new card
-						</Button>
-						<BasicModal open={open}
-						            handleOpen={() => setOpen(true)}
-						            handleClose={() => setOpen(false)}
-						            title={'Add new card'}>
-								<div style={{display: 'flex', flexDirection: 'column', gap: '40px'}}>
-										<div>
-												<TextField value={question}
-												           onChange={(e) => setQuestion(e.currentTarget.value)}
-												           variant="standard"
-												           label="Question"
-												           fullWidth
-												           style={{marginBottom: '15px'}}/>
-												<TextField value={answer}
-												           onChange={(e) => setAnswer(e.currentTarget.value)}
-												           variant="standard"
-												           label="Answer" fullWidth/>
-										</div>
-										<div style={{display: 'flex', justifyContent: 'space-between'}}>
-												<Button variant="outlined" onClick={() => setOpen(false)}>Cancel</Button>
-												<Button variant="contained" onClick={handleSave}>Save</Button>
-										</div>
-								</div>
-						</BasicModal>
-				</div>
-		)
+            if (file.size < 4000000) {
+                // https://developer.mozilla.org/ru/docs/Web/API/FileReader/FileReader
+                const reader = new FileReader();
+
+                reader.onloadend = () => {
+                    const file64 = reader.result as string
+                    setQuestionImg(file64)
+                }
+                // https://developer.mozilla.org/ru/docs/Web/API/FileReader/readAsDataURL
+                reader.readAsDataURL(file)
+            } else {
+                console.error('Error: ', 'Файл слишком большого размера')
+            }
+        }
+    }
+
+
+    const handleSave = async () => {
+        if (questionFormat === 'Text') {
+            await dispatch(createCard({cardsPack_id: packId, answer, question}))
+        }
+        if (questionFormat === 'Image') {
+            await dispatch(createCard({cardsPack_id: packId, questionImg, answer}))
+        }
+        await dispatch(fetchCards(packId))
+        setOpen(false)
+        setQuestion('')
+        setAnswer('')
+    }
+    return (
+        <div>
+            <Button variant="contained" onClick={() => setOpen(true)}>
+                Add new card
+            </Button>
+            <BasicModal open={open}
+                        handleOpen={() => setOpen(true)}
+                        handleClose={() => setOpen(false)}
+                        title={'Add new card'}>
+                <div style={{display: 'flex', flexDirection: 'column', gap: '40px'}}>
+                    <div>
+                        <FormControl sx={{margin: '30px 0'}} fullWidth>
+                            <InputLabel id="select-label">Choose a question format</InputLabel>
+                            <Select
+                                labelId="select-label"
+                                id="demo-simple-select"
+                                defaultValue={questionFormat}
+                                value={questionFormat}
+                                size='small'
+                                label="Choose a question format"
+                                onChange={(e) => setQuestionFormat(e.target.value)}
+                            >
+                                <MenuItem value='Text'>Text</MenuItem>
+                                <MenuItem value='Image'>Image</MenuItem>
+                            </Select>
+                        </FormControl>
+                        {questionFormat === 'Text'
+                            ? <>
+                                <TextField value={question}
+                                           onChange={(e) => setQuestion(e.currentTarget.value)}
+                                           variant="standard"
+                                           label="Question"
+                                           fullWidth
+                                           style={{marginBottom: '15px'}}/>
+                                <TextField value={answer}
+                                           onChange={(e) => setAnswer(e.currentTarget.value)}
+                                           variant="standard"
+                                           label="Answer" fullWidth/>
+                            </>
+                            : <>
+                                {questionImg && <img style={{width: '100%'}} src={questionImg} alt="question image"/>}
+                                <Button fullWidth variant="contained" component="label">
+                                    Upload question image
+                                    <input onChange={uploadHandler} hidden accept="image/*" multiple type="file"/>
+                                </Button>
+                                <TextField value={answer}
+                                           onChange={(e) => setAnswer(e.currentTarget.value)}
+                                           variant="standard"
+                                           label="Answer" fullWidth/>
+                            </>
+                        }
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Button variant="outlined" onClick={() => setOpen(false)}>Cancel</Button>
+                        <Button variant="contained" onClick={handleSave}>Save</Button>
+                    </div>
+                </div>
+            </BasicModal>
+        </div>
+    )
 }
