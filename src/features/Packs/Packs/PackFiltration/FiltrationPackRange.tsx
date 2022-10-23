2761ba2a306fea40f77a2d9d2e6446cce0d59170
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-10-23 14:49:38 +0300

    implemented upload deck cover image

--- src/features/Packs/Packs/PackFiltration/FiltrationPackRange.tsx
+++ src/features/Packs/Packs/PackFiltration/FiltrationPackRange.tsx
@@ -5,63 +5,63 @@ import {useAppDispatch, useAppSelector} from 'app/store';
 import {setPackSearchParams} from 'features/Packs/packs-reducer';
 
 export const FiltrationPackRange = () => {
-		const dispatch = useAppDispatch()
-		const minCardsCountFromState = useAppSelector(state => state.packs.packsState.minCardsCount)
-		const maxCardsCountFromState = useAppSelector(state => state.packs.packsState.maxCardsCount)
-		const {min, max, user_id} = useAppSelector(state => state.packs.searchParams)
-		const [value, setValue] = React.useState<number[]>([0, 0])
-		const debouncedValue = useDebounce(value, 1000)
-		const [isRangeTouched, setIsRangeTouched] = useState(false)
+    const dispatch = useAppDispatch()
+    const minCardsCountFromState = useAppSelector(state => state.packs.packsState.minCardsCount)
+    const maxCardsCountFromState = useAppSelector(state => state.packs.packsState.maxCardsCount)
+    const {min, max, user_id} = useAppSelector(state => state.packs.searchParams)
+    const [value, setValue] = React.useState<number[]>([0, 0])
+    const debouncedValue = useDebounce(value, 1000)
+    const [isRangeTouched, setIsRangeTouched] = useState(false)
 
-		const handleRangeChange = (event: Event, newValue: number | number[], activeThumb: number) => {
-				if (activeThumb >= 0) {
-						setIsRangeTouched(true)
-				}
-				setValue(newValue as number[])
-		}
-
-
-		useEffect(() => {
-				//when click clear filters reset range
-				if (min === minCardsCountFromState && max === maxCardsCountFromState) {
-						setValue([min, max])
-				}
-		}, [min, max])
-
-		useEffect(() => {
-				//when max value from server got set to range
-				setValue([minCardsCountFromState, maxCardsCountFromState])
-		}, [maxCardsCountFromState])
-
-		useEffect(() => {
-				if (user_id) {
-						setIsRangeTouched(false)
-				}
-				if (isRangeTouched) {
-						dispatch(setPackSearchParams({min: value[0], max: value[1]}))
-				}
-		}, [debouncedValue, user_id])
+    const handleRangeChange = (event: Event, newValue: number | number[], activeThumb: number) => {
+        if (activeThumb >= 0) {
+            setIsRangeTouched(true)
+        }
+        setValue(newValue as number[])
+    }
 
 
-		return (
-				<div style={{width: '220px'}}>
-						<h3 style={{fontWeight: '500', fontSize: '14px'}}>Number of cards</h3>
-						<div style={{display: 'flex', justifyContent: 'space-between', gap: '15px'}}>
-								<Paper style={{minWidth: '36px', height: '36px', textAlign: 'center', lineHeight: '36px'}}>
-										{minCardsCountFromState}
-								</Paper>
-								<Slider value={value}
-								        onChange={handleRangeChange}
-								        valueLabelDisplay="auto"
-								        min={minCardsCountFromState}
-								        max={maxCardsCountFromState}
-								        disableSwap
-								        disabled={maxCardsCountFromState === 0}
-								/>
-								<Paper style={{minWidth: '36px', height: '36px', textAlign: 'center', lineHeight: '36px'}}>
-										{maxCardsCountFromState}
-								</Paper>
-						</div>
-				</div>
-		);
+    useEffect(() => {
+        //when click clear filters reset range
+        if (min === minCardsCountFromState && max === maxCardsCountFromState) {
+            setValue([min, max])
+        }
+    }, [min, max])
+
+    useEffect(() => {
+        //when max value from server got set to range
+        setValue([minCardsCountFromState, maxCardsCountFromState])
+    }, [maxCardsCountFromState])
+
+    useEffect(() => {
+        if (user_id) {
+            setIsRangeTouched(false)
+        }
+        if (isRangeTouched) {
+            dispatch(setPackSearchParams({min: value[0], max: value[1]}))
+        }
+    }, [debouncedValue, user_id])
+
+
+    return (
+        <div style={{width: '220px'}}>
+            <h3 style={{fontWeight: '500', fontSize: '14px'}}>Number of cards</h3>
+            <div style={{display: 'flex', justifyContent: 'space-between', gap: '15px'}}>
+                <Paper style={{minWidth: '36px', height: '36px', textAlign: 'center', lineHeight: '36px'}}>
+                    {minCardsCountFromState}
+                </Paper>
+                <Slider value={value}
+                        onChange={handleRangeChange}
+                        valueLabelDisplay="auto"
+                        min={minCardsCountFromState}
+                        max={maxCardsCountFromState}
+                        disableSwap
+                        disabled={maxCardsCountFromState === 0 || minCardsCountFromState === maxCardsCountFromState}
+                />
+                <Paper style={{minWidth: '36px', height: '36px', textAlign: 'center', lineHeight: '36px'}}>
+                    {maxCardsCountFromState}
+                </Paper>
+            </div>
+        </div>
+    );
 };
--- src/features/Packs/PacksModals/AddPackModal.tsx
+++ src/features/Packs/PacksModals/AddPackModal.tsx
@@ -1,53 +1,79 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {BasicModal} from 'features/Modals/BasicModal';
 import {Button, Checkbox, FormControlLabel, TextField} from '@mui/material';
 import {useAppDispatch} from 'app/store';
 import {createPack, fetchPacks} from 'features/Packs/packs-reducer';
 
 export const AddPackModal = () => {
-		const dispatch = useAppDispatch()
-		const [value, setValue] = useState('')
-		const [checked, setChecked] = useState(false)
-		const [open, setOpen] = useState(false)
+    const dispatch = useAppDispatch()
+    const [value, setValue] = useState('')
+    const [checked, setChecked] = useState(false)
+    const [open, setOpen] = useState(false)
+    const [deckCover, setDeckCover] = useState('')
 
-		const handleClose = () => {
-				setOpen(false)
-				setValue('')
-				setChecked(false)
-		}
+    const handleClose = () => {
+        setOpen(false)
+        setValue('')
+        setChecked(false)
+    }
 
-		const handleSave = async () => {
-				await dispatch(createPack({name: value, private: checked, deckCover: ''}))
-				await dispatch(fetchPacks())
-				handleClose()
-		}
+    const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length) {
+            const file = e.target.files[0]
 
-		return (
-				<div>
-						<Button onClick={()=>setOpen(true)} variant="contained">Add New Pack</Button>
-						<BasicModal open={open}
-						            handleOpen={()=>setOpen(true)}
-						            handleClose={handleClose}
-						            title={'Add new pack'}
-						>
-								<div>
-										<TextField fullWidth
-										           label="Name Pack"
-										           variant="standard"
-										           value={value}
-										           onChange={(e) => setValue(e.currentTarget.value)}
-										/>
-										<FormControlLabel control={<Checkbox checked={checked}
-										                                     onChange={(e) => setChecked(e.currentTarget.checked)}/>}
-										                  label="Private pack"
-										                  style={{margin: '30px 0'}}
-										/>
-										<div style={{display: 'flex', justifyContent: 'space-between'}}>
-												<Button variant="outlined" onClick={handleClose}>Cancel</Button>
-												<Button variant="contained" onClick={handleSave}>Save</Button>
-										</div>
-								</div>
-						</BasicModal>
-				</div>
-		);
+            if (file.size < 4000000) {
+                // https://developer.mozilla.org/ru/docs/Web/API/FileReader/FileReader
+                const reader = new FileReader();
+
+                reader.onloadend = () => {
+                    const file64 = reader.result as string
+                    setDeckCover(file64)
+                }
+                // https://developer.mozilla.org/ru/docs/Web/API/FileReader/readAsDataURL
+                reader.readAsDataURL(file)
+            } else {
+                console.error('Error: ', 'Файл слишком большого размера')
+            }
+        }
+    }
+
+    const handleSave = async () => {
+        await dispatch(createPack({name: value, private: checked, deckCover}))
+        await dispatch(fetchPacks())
+        handleClose()
+    }
+
+    return (
+        <div>
+            <Button onClick={() => setOpen(true)} variant="contained">Add New Pack</Button>
+            <BasicModal open={open}
+                        handleOpen={() => setOpen(true)}
+                        handleClose={handleClose}
+                        title={'Add new pack'}
+            >
+                <div>
+                    {deckCover && <img style={{maxWidth: '100%'}} src={deckCover} alt="deck cover"/>}
+                    <Button sx={{margin: '20px 0'}} variant="contained" component="label" fullWidth>
+                        Upload deck cover
+                        <input onChange={uploadHandler} hidden accept="image/*" multiple type="file"/>
+                    </Button>
+                    <TextField fullWidth
+                               label="Name Pack"
+                               variant="standard"
+                               value={value}
+                               onChange={(e) => setValue(e.currentTarget.value)}
+                    />
+                    <FormControlLabel control={<Checkbox checked={checked}
+                                                         onChange={(e) => setChecked(e.currentTarget.checked)}/>}
+                                      label="Private pack"
+                                      style={{margin: '30px 0'}}
+                    />
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+                        <Button variant="contained" onClick={handleSave}>Save</Button>
+                    </div>
+                </div>
+            </BasicModal>
+        </div>
+    );
 };
