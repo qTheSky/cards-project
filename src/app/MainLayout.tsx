9bd480466c6dcc4ba46598fece1d72200197f34b
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-10-25 17:39:18 +0300

    refactor: disable backdrop on learnPage

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Outlet} from 'react-router-dom';
+import {Outlet, useLocation} from 'react-router-dom';
 import {Header} from 'common/components/Header/Header';
 import {ErrorSnackbar} from 'common/components/ErrorSnackBar';
 import {useAppSelector} from 'app/store';
 import {getIsAppAppMakingRequest} from 'app/selectors';
 import {BackDrop} from 'common/components/BackDrop/BackDrop';
+import {PATH} from 'app/RouteVariables';
 
 export const MainLayout = () => {
 		const isAppMakeRequest = useAppSelector(getIsAppAppMakingRequest)
+		const {pathname} = useLocation()
 		return (
 				<>
 						<Header/>
 						<Outlet/>
 						<ErrorSnackbar/>
-						{isAppMakeRequest && <BackDrop open={isAppMakeRequest}/>}
+						{(isAppMakeRequest && !pathname.includes(PATH.learn)) && <BackDrop open={isAppMakeRequest}/>}
 				</>
 		);
 };
