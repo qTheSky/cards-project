e09ac50743840741afb88a54fc90425001f069cc
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-10-26 22:35:20 +0300

    refactor learnpage add spinner

--- src/features/Cards/Learn/LearnPage.tsx
+++ src/features/Cards/Learn/LearnPage.tsx
@@ -1,4 +1,4 @@
-import {Container} from '@mui/material';
+import {CircularProgress, Container} from '@mui/material';
 import React, {useEffect, useState} from 'react';
 import {BackToPackListLink} from 'common/components/BackToPackListArrow/BackToPackListLink';
 import {useAppDispatch, useAppSelector} from 'app/store';
@@ -12,48 +12,52 @@ import {LearnPaper} from 'features/Cards/Learn/LearnPaper';
 
 
 export const LearnPage = () => {
-		const dispatch = useAppDispatch()
-		const {packId} = useParams() as { packId: string }
-		const isLoggedIn = useAppSelector(getIsLoggedIn)
-		const packName = useAppSelector(getPackName)
-		const cards = useAppSelector(getCards)
-		const [displayedCard, setDisplayedCard] = useState<any>({})
+    const dispatch = useAppDispatch()
+    const {packId} = useParams() as { packId: string }
+    const isLoggedIn = useAppSelector(getIsLoggedIn)
+    const packName = useAppSelector(getPackName)
+    const cards = useAppSelector(getCards)
+    const [displayedCard, setDisplayedCard] = useState<any>({})
 
 
-		useEffect(() => {
-				return () => {
-						dispatch(setCardsSearchParams({pageCount: 10}))
-						dispatch(resetCardsState())
-						setDisplayedCard({})
-				}
-		}, [])
+    useEffect(() => {
+        return () => {
+            dispatch(setCardsSearchParams({pageCount: 10}))
+            dispatch(resetCardsState())
+            setDisplayedCard({})
+        }
+    }, [])
 
-		useEffect(() => {
-				dispatch(setCardsSearchParams({pageCount: 99999}))
-				dispatch(fetchCards(packId))
-		}, [])
+    useEffect(() => {
+        dispatch(setCardsSearchParams({pageCount: 99999}))
+        dispatch(fetchCards(packId))
+    }, [])
 
 
-		useEffect(() => {
-				//when first render set card to state
-				if (cards.length && !displayedCard._id) {
-						setDisplayedCard(getNextCardToLearn(cards))
-				}
-				//when card.shots changed set it to state it needs when user click next question attempts count increments
-				const card = cards.find(card => card._id === displayedCard._id)
-				if (card) {
-						setDisplayedCard(card)
-				}
-		}, [cards])
+    useEffect(() => {
+        if (!!packName && !displayedCard._id) {
+            setDisplayedCard(getNextCardToLearn(cards))
+        }
+        //when card.shots changed set it to state it needs when user click next question shots count increments at once
+        const card = cards.find(card => card._id === displayedCard._id)
+        if (card) {
+            setDisplayedCard(card)
+        }
+    }, [cards])
 
-		if (!isLoggedIn) return <Navigate to={PATH.login}/>
-		return (
-				<div>
-						<Container>
-								<BackToPackListLink/>
-								<h1 style={{textAlign: 'center', marginBottom: '20px'}}>Learn ''{packName || 'loading...'}''</h1>
-								<LearnPaper displayedCard={displayedCard} setDisplayedCard={setDisplayedCard}/>
-						</Container>
-				</div>
-		)
+    if (!isLoggedIn) return <Navigate to={PATH.login}/>
+    return (
+        <div>
+            <Container>
+                <BackToPackListLink/>
+                <h1 style={{textAlign: 'center', marginBottom: '20px'}}>{packName ? packName : 'loading...'}</h1>
+                {packName
+                    ? <LearnPaper displayedCard={displayedCard} setDisplayedCard={setDisplayedCard}/>
+                    : <div style={{marginTop: '60px', textAlign: 'center', width: '100%'}}>
+                        <CircularProgress/>
+                    </div>
+                }
+            </Container>
+        </div>
+    )
 }
